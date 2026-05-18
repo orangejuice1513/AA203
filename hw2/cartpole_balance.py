1095ae542156030635bbaa3f15e6f3eb1ddc31d9
Julia <juliaj94@stanford.edu>
2026-05-17 21:21:59 -0700

    Track hw2 files as normal directory instead of submodule

---
+++ hw2/cartpole_balance.py
@@ -0,0 +1,240 @@
+"""
+Solution code for the problem "Cart-pole balance".
+
+Autonomous Systems Lab (ASL), Stanford University
+"""
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+from animations import animate_cartpole
+
+# Constants
+n = 4  # state dimension
+m = 1  # control dimension
+mp = 2.0  # pendulum mass
+mc = 10.0  # cart mass
+L = 1.0  # pendulum length
+g = 9.81  # gravitational acceleration
+dt = 0.1  # discretization time step
+animate = False  # whether or not to animate results
+
+
+def cartpole(s: np.ndarray, u: np.ndarray) -> np.ndarray:
+    """Compute the cart-pole state derivative
+
+    Args:
+        s (np.ndarray): The cartpole state: [x, theta, x_dot, theta_dot], shape (n,)
+        u (np.ndarray): The cartpole control: [F_x], shape (m,)
+
+    Returns:
+        np.ndarray: The state derivative, shape (n,)
+    """
+    x, θ, dx, dθ = s
+    sinθ, cosθ = np.sin(θ), np.cos(θ)
+    h = mc + mp * (sinθ**2)
+    ds = np.array(
+        [
+            dx,
+            dθ,
+            (mp * sinθ * (L * (dθ**2) + g * cosθ) + u[0]) / h,
+            -((mc + mp) * g * sinθ + mp * L * (dθ**2) * sinθ * cosθ + u[0] * cosθ)
+            / (h * L),
+        ]
+    )
+    return ds
+
+
+def reference(t: float) -> np.ndarray:
+    """Compute the reference state (s_bar) at time t
+
+    Args:
+        t (float): Evaluation time
+
+    Returns:
+        np.ndarray: Reference state, shape (n,)
+    """
+    a = 10.0  # Amplitude
+    T = 10.0  # Period
+
+    # PART (d) ##################################################
+    # INSTRUCTIONS: Compute the reference state for a given time
+    raise NotImplementedError()
+    # END PART (d) ##############################################
+
+
+def ricatti_recursion(
+    A: np.ndarray, B: np.ndarray, Q: np.ndarray, R: np.ndarray
+) -> np.ndarray:
+    """Compute the gain matrix K through Ricatti recursion
+
+    Args:
+        A (np.ndarray): Dynamics matrix, shape (n, n)
+        B (np.ndarray): Controls matrix, shape (n, m)
+        Q (np.ndarray): State cost matrix, shape (n, n)
+        R (np.ndarray): Control cost matrix, shape (m, m)
+
+    Returns:
+        np.ndarray: Gain matrix K, shape (m, n)
+    """
+    eps = 1e-4  # Riccati recursion convergence tolerance
+    max_iters = 1000  # Riccati recursion maximum number of iterations
+    P_prev = np.zeros((n, n))  # initialization
+    converged = False
+    for i in range(max_iters):
+        # PART (b) ##################################################
+        # INSTRUCTIONS: Apply the Ricatti equation until convergence
+        K = -np.linalg.inv(R + B.T @ P_prev @ B) @ B.T @ P_prev @ A
+        P = Q + A.T @ P_prev @ (A + B @ K)
+        if (np.max(np.abs(P - P_prev))) < eps:
+            converged = True
+            break 
+        P_prev = P 
+        # END PART (b) ##############################################
+    if not converged:
+        raise RuntimeError("Ricatti recursion did not converge!")
+    print("K:", np.round(K, decimals=2))
+    return K
+
+
+def simulate(
+    t: np.ndarray, s_ref: np.ndarray, u_ref: np.ndarray, s0: np.ndarray, K: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
+    """Simulate the cartpole
+
+    Args:
+        t (np.ndarray): Evaluation times, shape (num_timesteps,)
+        s_ref (np.ndarray): Reference state s_bar, evaluated at each time t. Shape (num_timesteps, n)
+        u_ref (np.ndarray): Reference control u_bar, shape (m,)
+        s0 (np.ndarray): Initial state, shape (n,)
+        K (np.ndarray): Feedback gain matrix (Ricatti recursion result), shape (m, n)
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: Tuple of:
+            np.ndarray: The state history, shape (num_timesteps, n)
+            np.ndarray: The control history, shape (num_timesteps, m)
+    """
+
+    def cartpole_wrapper(s, t, u):
+        """Helper function to get cartpole() into a form preferred by odeint, which expects t as the second arg"""
+        return cartpole(s, u)
+
+    # PART (c) ##################################################
+    # INSTRUCTIONS: Complete the function to simulate the cartpole system
+    # Hint: use the cartpole wrapper above with odeint
+
+    # initialize the s and u arrays 
+    s = np.zeros((len(t), 4))
+    u = np.zeros((len(t), 1))
+
+    s[0] = s0 # initial state
+
+    for k in range(len(t) - 1):
+        s_tilde = s[k] - s_ref[k] # error 
+        u[k] = u_ref + K @ s_tilde
+        s[k+1] = odeint(cartpole_wrapper, s[k], t[k:k+2], args=(u[k],))[1] 
+    
+    # END PART (c) ##############################################
+    return s, u
+
+
+def compute_lti_matrices() -> tuple[np.ndarray, np.ndarray]:
+    """Compute the linearized dynamics matrices A and B of the LTI system
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: Tuple of:
+            np.ndarray: The A (dynamics) matrix, shape (n, n)
+            np.ndarray: The B (controls) matrix, shape (n, m)
+    """
+    # PART (a) ##################################################
+    # INSTRUCTIONS: Construct the A and B matrices
+    A = A = np.array([
+    [1.0, 0.0, dt, 0.0],
+    [0.0, 1.0, 0.0, dt],
+    [0.0, dt * (mp * g) / mc, 1.0, 0.0],
+    [0.0, dt * (mc + mp) * g / (mc * L), 0.0, 1.0]
+    ])
+    B = B = np.array([
+    [0.0],
+    [0.0],
+    [dt / mc],
+    [dt / (mc * L)]
+    ])
+    # END PART (a) ##############################################
+    return A, B
+
+
+def plot_state_and_control_history(
+    s: np.ndarray, u: np.ndarray, t: np.ndarray, s_ref: np.ndarray, name: str
+) -> None:
+    """Helper function for cartpole visualization
+
+    Args:
+        s (np.ndarray): State history, shape (num_timesteps, n)
+        u (np.ndarray): Control history, shape (num_timesteps, m)
+        t (np.ndarray): Times, shape (num_timesteps,)
+        s_ref (np.ndarray): Reference state s_bar, evaluated at each time t. Shape (num_timesteps, n)
+        name (str): Filename prefix for saving figures
+    """
+    fig, axes = plt.subplots(1, n + m, dpi=150, figsize=(15, 2))
+    plt.subplots_adjust(wspace=0.35)
+    labels_s = (r"$x(t)$", r"$\theta(t)$", r"$\dot{x}(t)$", r"$\dot{\theta}(t)$")
+    labels_u = (r"$u(t)$",)
+    for i in range(n):
+        axes[i].plot(t, s[:, i])
+        axes[i].plot(t, s_ref[:, i], "--")
+        axes[i].set_xlabel(r"$t$")
+        axes[i].set_ylabel(labels_s[i])
+    for i in range(m):
+        axes[n + i].plot(t, u[:, i])
+        axes[n + i].set_xlabel(r"$t$")
+        axes[n + i].set_ylabel(labels_u[i])
+    plt.savefig(f"{name}.png", bbox_inches="tight")
+    plt.show()
+
+    if animate:
+        fig, ani = animate_cartpole(t, s[:, 0], s[:, 1])
+        ani.save(f"{name}.mp4", writer="ffmpeg")
+        plt.show()
+
+
+def main():
+    # Part A
+    A, B = compute_lti_matrices()
+
+    # Part B
+    Q = np.eye(n) * 10 # state cost matrix
+    R = np.eye(m)  # control cost matrix
+    K = ricatti_recursion(A, B, Q, R) 
+
+    # # Part C
+    t = np.arange(0.0, 30.0, 1 / 10)
+    s_ref = np.array([0.0, np.pi, 0.0, 0.0]) * np.ones((t.size, 1))
+    u_ref = np.array([0.0])
+    s0 = np.array([0.0, 3 * np.pi / 4, 0.0, 0.0])
+    s, u = simulate(t, s_ref, u_ref, s0, K)
+    # plot_state_and_control_history(s, u, t, s_ref, "cartpole_balance")
+
+    # # Part D 
+    # Note: t, u_ref unchanged from part c 
+
+    def reference(ti: float) -> np.ndarray:  
+        # new s_ref calculation from pt d  
+        a = 10.0 
+        T = 10.0 
+        return np.array([ 
+            a * np.sin(2*np.pi*ti/T), 
+            np.pi, 
+            (2*np.pi*a/T) * np.cos(2*np.pi*ti/T), 
+            0.0, 
+        ]) 
+
+    s_ref = np.array([reference(ti) for ti in t]) 
+    s0 = np.array([0.0, np.pi, 0.0, 0.0]) 
+    s, u = simulate(t, s_ref, u_ref, s0, K) 
+    plot_state_and_control_history(s, u, t, s_ref, "cartpole_balance_tv") 
+
+
+if __name__ == "__main__":
+    main()
---
+++ hw2/cartpole_swingup.py
@@ -0,0 +1,246 @@
+"""
+Starter code for the problem "Cart-pole swing-up".
+
+Autonomous Systems Lab (ASL), Stanford University
+"""
+
+import time
+
+from animations import animate_cartpole
+
+import jax
+import jax.numpy as jnp
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from scipy.integrate import odeint
+
+
+def linearize(f, s, u):
+    """Linearize the function `f(s, u)` around `(s, u)`.
+
+    Arguments
+    ---------
+    f : callable
+        A nonlinear function with call signature `f(s, u)`.
+    s : numpy.ndarray
+        The state (1-D).
+    u : numpy.ndarray
+        The control input (1-D).
+
+    Returns
+    -------
+    A : numpy.ndarray
+        The Jacobian of `f` at `(s, u)`, with respect to `s`.
+    B : numpy.ndarray
+        The Jacobian of `f` at `(s, u)`, with respect to `u`.
+    """
+    # WRITE YOUR CODE BELOW ###################################################
+    # INSTRUCTIONS: Use JAX to compute `A` and `B` in one line.
+    A, B = jax.jacobian(f, argnums = (0, 1))(s, u)
+    ###########################################################################
+    return A, B
+
+
+def ilqr(f, s0, s_goal, N, Q, R, QN, eps=1e-3, max_iters=1000):
+    """Compute the iLQR set-point tracking solution.
+
+    Arguments
+    ---------
+    f : callable
+        A function describing the discrete-time dynamics, such that
+        `s[k+1] = f(s[k], u[k])`.
+    s0 : numpy.ndarray
+        The initial state (1-D).
+    s_goal : numpy.ndarray
+        The goal state (1-D).
+    N : int
+        The time horizon of the LQR cost function.
+    Q : numpy.ndarray
+        The state cost matrix (2-D).
+    R : numpy.ndarray
+        The control cost matrix (2-D).
+    QN : numpy.ndarray
+        The terminal state cost matrix (2-D).
+    eps : float, optional
+        Termination threshold for iLQR.
+    max_iters : int, optional
+        Maximum number of iLQR iterations.
+
+    Returns
+    -------
+    s_bar : numpy.ndarray
+        A 2-D array where `s_bar[k]` is the nominal state at time step `k`,
+        for `k = 0, 1, ..., N-1`
+    u_bar : numpy.ndarray
+        A 2-D array where `u_bar[k]` is the nominal control at time step `k`,
+        for `k = 0, 1, ..., N-1`
+    Y : numpy.ndarray
+        A 3-D array where `Y[k]` is the matrix gain term of the iLQR control
+        law at time step `k`, for `k = 0, 1, ..., N-1`
+    y : numpy.ndarray
+        A 2-D array where `y[k]` is the offset term of the iLQR control law
+        at time step `k`, for `k = 0, 1, ..., N-1`
+    """
+    if max_iters <= 1:
+        raise ValueError("Argument `max_iters` must be at least 1.")
+    n = Q.shape[0]  # state dimension
+    m = R.shape[0]  # control dimension
+
+    # Initialize gains `Y` and offsets `y` for the policy
+    Y = np.zeros((N, m, n))
+    y = np.zeros((N, m))
+
+    # Initialize the nominal trajectory `(s_bar, u_bar`), and the
+    # deviations `(ds, du)`
+    u_bar = np.zeros((N, m))
+    s_bar = np.zeros((N + 1, n))
+    s_bar[0] = s0
+    for k in range(N):
+        s_bar[k + 1] = f(s_bar[k], u_bar[k])
+    ds = np.zeros((N + 1, n))
+    du = np.zeros((N, m))
+
+    # iLQR loop
+    converged = False
+    for _ in range(max_iters):
+        # Linearize the dynamics at each step `k` of `(s_bar, u_bar)`
+        A, B = jax.vmap(linearize, in_axes=(None, 0, 0))(f, s_bar[:-1], u_bar)
+        A, B = np.array(A), np.array(B)
+
+        # PART (c) ############################################################
+        # INSTRUCTIONS: Update `Y`, `y`, `ds`, `du`, `s_bar`, and `u_bar`.
+        P = QN.copy()
+        p = QN @ (s_bar[-1] - s_goal)
+
+        # step back from N-1 down to 0
+        for k in range(N - 1, -1, -1):
+            q_k = Q @ (s_bar[k] - s_goal)
+            r_k = R @ u_bar[k]
+            
+            Q_xx = Q + A[k].T @ P @ A[k]
+            Q_uu = R + B[k].T @ P @ B[k]
+            Q_ux = B[k].T @ P @ A[k]
+            Q_x = q_k + A[k].T @ p
+            Q_u = r_k + B[k].T @ p
+            
+            Y[k] = -np.linalg.solve(Q_uu, Q_ux)
+            y[k] = -np.linalg.solve(Q_uu, Q_u)
+            
+            # next cost to go 
+            P = Q_xx + Q_ux.T @ Y[k]
+            p = Q_x + Q_ux.T @ y[k]
+            
+            
+        # step forward from 0 to N-1
+        for k in range(N):
+            du[k] = Y[k] @ ds[k] + y[k]
+            u_bar[k] = u_bar[k] + du[k]
+            new_s = f(s_bar[k], u_bar[k])
+            ds[k+1] = new_s - s_bar[k+1]
+            
+            s_bar[k+1] = new_s # update nominal state trajectory 
+            
+        #######################################################################
+
+        if np.max(np.abs(du)) < eps:
+            converged = True
+            break
+    if not converged:
+        raise RuntimeError("iLQR did not converge!")
+    return s_bar, u_bar, Y, y
+
+
+def cartpole(s, u):
+    """Compute the cart-pole state derivative."""
+    mp = 2.0  # pendulum mass
+    mc = 10.0  # cart mass
+    L = 1.0  # pendulum length
+    g = 9.81  # gravitational acceleration
+
+    x, θ, dx, dθ = s
+    sinθ, cosθ = jnp.sin(θ), jnp.cos(θ)
+    h = mc + mp * (sinθ**2)
+    ds = jnp.array(
+        [
+            dx,
+            dθ,
+            (mp * sinθ * (L * (dθ**2) + g * cosθ) + u[0]) / h,
+            -((mc + mp) * g * sinθ + mp * L * (dθ**2) * sinθ * cosθ + u[0] * cosθ)
+            / (h * L),
+        ]
+    )
+    return ds
+
+
+# Define constants
+n = 4  # state dimension
+m = 1  # control dimension
+Q = np.diag(np.array([10.0, 10.0, 2.0, 2.0]))  # state cost matrix
+R = 1e-2 * np.eye(m)  # control cost matrix
+QN = 1e2 * np.eye(n)  # terminal state cost matrix
+s0 = np.array([0.0, 0.0, 0.0, 0.0])  # initial state
+s_goal = np.array([0.0, np.pi, 0.0, 0.0])  # goal state
+T = 10.0  # simulation time
+dt = 0.1  # sampling time
+animate = False  # flag for animation
+closed_loop = True  # flag for closed-loop control
+
+# Initialize continuous-time and discretized dynamics
+f = jax.jit(cartpole)
+fd = jax.jit(lambda s, u, dt=dt: s + dt * f(s, u))
+
+# Compute the iLQR solution with the discretized dynamics
+print("Computing iLQR solution ... ", end="", flush=True)
+start = time.time()
+t = np.arange(0.0, T, dt)
+N = t.size - 1
+s_bar, u_bar, Y, y = ilqr(fd, s0, s_goal, N, Q, R, QN)
+print("done! ({:.2f} s)".format(time.time() - start), flush=True)
+
+# Simulate on the true continuous-time system
+print("Simulating ... ", end="", flush=True)
+start = time.time()
+s = np.zeros((N + 1, n))
+u = np.zeros((N, m))
+s[0] = s0
+for k in range(N):
+    # PART (d) ################################################################
+    # INSTRUCTIONS: Compute either the closed-loop or open-loop value of
+    # `u[k]`, depending on the Boolean flag `closed_loop`.
+    if closed_loop:
+        u[k] = 0.0
+        ds_k = s[k] - s_bar[k]
+        u[k] = u_bar[k] + Y[k] @ ds_k
+    else:  # do open-loop control
+        u[k] = 0.0
+        u[k] = u_bar[k]
+    ###########################################################################
+    s[k + 1] = odeint(lambda s, t: f(s, u[k]), s[k], t[k : k + 2])[1]
+print("done! ({:.2f} s)".format(time.time() - start), flush=True)
+
+# Plot
+fig, axes = plt.subplots(1, n + m, dpi=150, figsize=(15, 2))
+plt.subplots_adjust(wspace=0.45)
+labels_s = (r"$x(t)$", r"$\theta(t)$", r"$\dot{x}(t)$", r"$\dot{\theta}(t)$")
+labels_u = (r"$u(t)$",)
+for i in range(n):
+    axes[i].plot(t, s[:, i])
+    axes[i].set_xlabel(r"$t$")
+    axes[i].set_ylabel(labels_s[i])
+for i in range(m):
+    axes[n + i].plot(t[:-1], u[:, i])
+    axes[n + i].set_xlabel(r"$t$")
+    axes[n + i].set_ylabel(labels_u[i])
+if closed_loop:
+    plt.savefig("cartpole_swingup_cl.png", bbox_inches="tight")
+else:
+    plt.savefig("cartpole_swingup_ol.png", bbox_inches="tight")
+plt.show()
+
+if animate:
+    fig, ani = animate_cartpole(t, s[:, 0], s[:, 1])
+    ani.save("cartpole_swingup.mp4", writer="ffmpeg")
+    plt.show()
